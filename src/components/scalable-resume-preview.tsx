e45a85eaea79c7b5382e56ac918597d352fbae8a
E Akash Goud <letsmail.akashgoud@gmail.com>
2025-09-09 06:18:10 +0000

    now it looks like a zoomed one

--- src/components/scalable-resume-preview.tsx
+++ src/components/scalable-resume-preview.tsx
@@ -23,41 +23,10 @@ export function ScalableResumePreview({ resumeData, templateId }: ScalableResume
             </div>
             <style jsx>{`
                 .relative {
-                    --scale-factor: calc(100% / 1123px);
+                    --scale-factor: calc(100% / 794px);
                 }
-                @media (min-width: 640px) {
-                    .relative {
-                       --scale-factor: calc(100% / 1123px);
-                    }
-                }
-                @media (min-width: 768px) {
-                    .relative {
-                        --scale-factor: calc(100% / 1123px);
-                    }
-                }
-                @media (min-width: 1024px) {
-                     .relative {
-                        --scale-factor: calc(100% / 1123px);
-                    }
-                }
-                 @media (min-width: 1280px) {
-                     .relative {
-                        --scale-factor: calc(100% / 1123px);
-                    }
-                }
-                @media (min-width: 1536px) {
-                     .relative {
-                        --scale-factor: calc(100% / 1123px);
-                    }
-                }
-                
-                .w-full.h-full.relative{
+                .w-full.h-full.relative {
                     overflow: hidden;
-                }
-
-                .absolute {
-                    width: 794px;
-                    height: 1123px;
                 }
             `}</style>
         </div>
